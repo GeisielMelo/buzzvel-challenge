b512e5464aaf3e1f80a71e1173e89d07f2c59108
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-03-11 19:41:25 -0300

    feat: database connection handler

---
+++ backend/src/database/index.ts
@@ -0,0 +1,25 @@
+import mongoose, { Connection } from 'mongoose'
+import config from '../config'
+
+class Database {
+  connection!: Connection
+
+  async connect() {
+    try {
+      if (this.connection && this.connection.readyState === 1) return
+      await mongoose.connect(config.database.mongo.uri as string)
+    } catch (error) {
+      throw new Error('Database: Failed to connect.')
+    }
+  }
+
+  async disconnect() {
+    try {
+      await mongoose.disconnect()
+    } catch (error) {
+      throw new Error('Database: Failed to disconnect.')
+    }
+  }
+}
+
+export default new Database()
