e42943d67c48b4820059b8eaa3526f869b18548e
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-03-11 19:42:39 -0300

    feat: setup backend server start

---
+++ backend/src/app.ts
@@ -0,0 +1,39 @@
+import express, { Application, NextFunction, Request, Response } from 'express'
+import cors from 'cors'
+import routes from './routes'
+import corsOptions from './config/cors'
+
+export default class App {
+  app: Application
+
+  constructor() {
+    this.app = express()
+
+    this.middlewares()
+    this.routes()
+    this.exceptionHandler()
+  }
+
+  listen(port: number): void {
+    this.app.listen(port, () => {
+      console.log(`\x1b[32m[server] Server started on port \x1b[33m${port}\x1b[32m!\x1b[0m`)
+    })
+  }
+
+  private middlewares() {
+    this.app.use(express.json())
+    this.app.use(express.urlencoded({ extended: false }))
+    this.app.use(cors(corsOptions))
+  }
+
+  private routes() {
+    this.app.use(routes)
+  }
+
+  private exceptionHandler() {
+    this.app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+      console.log(`Exception handler: '${err.name} - ${err.message}'.`)
+      next()
+    })
+  }
+}
