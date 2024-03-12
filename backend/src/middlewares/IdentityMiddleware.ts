0dbece362fe62021c7973be3f59b22aaaf5221f6
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-03-12 15:24:05 -0300

    fix: changed response status value

--- backend/src/middlewares/IdentityMiddleware.ts
+++ backend/src/middlewares/IdentityMiddleware.ts
@@ -4,8 +4,8 @@ export default (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.params
   const pattern = /^[0-9a-fA-F]{24}$/
 
-  if (!id) return res.status(400).json({ message: 'Invalid id request' })
-  if (!pattern.test(id)) return res.status(500).json({ message: 'Internal server error' })
+  if (!id) return res.status(400).json({ message: 'Invalid id' })
+  if (!pattern.test(id)) return res.status(404).json({ message: 'Invalid id' })
 
   req.id = id
   return next()
