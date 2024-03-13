69ef745d17818044e537e59c215717a77e1bd766
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-03-12 22:09:00 -0300

    test: disabled download component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,7 @@ import { UpdateIcon } from '@radix-ui/react-icons'
 import Table from './components/Vacation'
 import Add from './components/Vacation/Add'
 import { useTable } from './context/TableContext'
-
-import Download from './components/Vacation/Download'
+// import Download from './components/Vacation/Download'
 
 const App: React.FC = () => {
   const { data, loading } = useTable()
@@ -13,7 +12,7 @@ const App: React.FC = () => {
       <main className='flex flex-col max-w-5xl w-full gap-4 p-4'>
         <div className='flex flex-col sm:flex-row gap-4 items-center w-full'>
           <h1 className='text-2xl font-bold'>Holiday Planning</h1>
-          <Download />
+          {/* <Download /> */}
           <Add />
         </div>
         <div className='flex justify-center p-6 border rounded-md'>
